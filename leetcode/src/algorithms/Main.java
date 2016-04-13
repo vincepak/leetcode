6111830116bc527b1841d8fecf6c6f0022ff1e45
I047020 <I047020@PVGN50853926A.apj.global.corp.sap>
2016-04-14 02:01:19 +0800

    comit

---
+++ leetcode/src/algorithms/Main.java
@@ -0,0 +1,12 @@
+package algorithms;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char dot = 56;
+		
+		System.out.println(Integer.parseInt(String.valueOf(dot)));
+
+	}
+
+}
---
+++ leetcode/unit-test/algorithms/ValidSudokuTest.java
@@ -0,0 +1,17 @@
+package algorithms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ValidSudokuTest {
+	ValidSudoku cut = new ValidSudoku();
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+		
+	}
+
+}
